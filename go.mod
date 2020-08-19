401b1a6690ed86d63a033ca588c5b1d7b4b7c33d
Klaus Post <klauspost@gmail.com>
2020-08-19 14:58:06 +0200

    Add hdfs test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/klauspost/test
+
+go 1.14
+
+require github.com/colinmarc/hdfs/v2 v2.1.1
---
+++ protoprob/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/colinmarc/hdfs/v2"
+)
+
+func main() {
+	fmt.Println("Initialized ok")
+}
